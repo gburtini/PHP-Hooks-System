5c7f0a853299300839719674f3f8c0e377195760
Giuseppe Burtini <joe@truephp.com>
2012-12-03 15:00:30 -0800

    Added test cases. Fought bugs. Won.

--- hook.php
+++ hook.php
@@ -1,6 +1,19 @@
 <?php
 	class Hooks {
-		public static $hooks = array();
+		protected static $hooks = array();
+
+		/**
+		 * clear(string $hook) - clears all callbacks associated with a given hook
+		 */
+		public static function clear($hook = null) {
+			if($hook === null) {
+                                foreach(self::$hooks as $key=>$value) {
+                                	self::clear($key);
+				}
+			} else {
+				unset(self::$hooks[$hook]);
+			}
+		}
 
 		/**
 	 	 * bind(string $hook, callback $callback, int $priority) - binds a callback to a hook
@@ -8,6 +21,9 @@
 		 * Associates a function with a given hook. Hooks are just strings as unique identifiers.
 		 */
 		public static function bind($hook, $callback, $priority = 10) {
+			if(!is_callable($callback))
+				return false;
+
 			if(!isset(self::$hooks[$hook]))
 				self::$hooks[$hook] = array();
 
@@ -15,6 +31,7 @@
 				self::$hooks[$hook][$priority] = array();
 
 			self::$hooks[$hook][$priority][] = $callback;			
+			return true;
 		}
 	
 		/**
@@ -26,11 +43,17 @@
 		public static function run($hook, $parameters=array()) {
 			self::sortHooks($hook);
 			$count = 0;
-			foreach(self::$hooks[$hook] as $hooks) {	
-				// hooks arrays will come in priority-sorted order here
-				foreach($hooks as $cb) {
-					if(@call_user_func_array($cb, $parameters) !== false) {
-						$count++;
+			if(isset(self::$hooks[$hook])) {
+				if(!is_array($parameters)) {
+					$parameters = func_get_args();
+					array_shift($parameters);
+				}
+				foreach(self::$hooks[$hook] as $hooks) {	
+					// hooks arrays will come in priority-sorted order here
+					foreach($hooks as $cb) {
+						if(@call_user_func_array($cb, $parameters) !== false) {
+							$count++;
+						}
 					}
 				}
 			}
@@ -46,10 +69,17 @@
 		 */
 		public static function filter($hook, $value, $parameters=array()) {
 			self::sortHooks($hook);
-			foreach(self::$hooks[$hook] as $hooks) {
-				foreach($hooks as $cb) {
-					array_unshift($value, $parameters);
-					$value = @call_user_func_array($cb, $parameters);
+                        if(isset(self::$hooks[$hook])) {
+				if(!is_array($parameters)) {
+					$parameters = func_get_args();
+					array_shift($parameters);
+					array_shift($parameters);
+				}
+				foreach(self::$hooks[$hook] as $hooks) {
+					foreach($hooks as $cb) {
+						array_unshift($parameters, $value);
+						$value = @call_user_func_array($cb, $parameters);
+					}
 				}
 			}
 			return $value;
@@ -71,7 +101,7 @@
 		 * 
 		 * http://svn.php.net/viewvc/php/php-src/trunk/Zend/zend_qsort.c?view=markup
 		 */
-		private static function sortHooks($hook = null) {
+		protected static function sortHooks($hook = null) {
 			if($hook === null) {
 				foreach(self::$hooks as $key=>$value) {
 					self::sortHooks($key);
@@ -79,7 +109,10 @@
 
 				return self::$hooks;
 			} else {
-				ksort(self::$hooks[$hook]);
+				if(isset(self::$hooks[$hook])) 
+					ksort(self::$hooks[$hook]);
+				else
+					return array();
 
 				return self::$hooks[$hook];
 			}
---
+++ tests/test_hook.php
@@ -0,0 +1,106 @@
+<?php
+	require_once "../hook.php";
+	function HookTestHelper_addOne($a) {
+		return $a+1;
+	}
+	function HookTestHelper_timesThree($a) {
+		return $a*3;
+	}
+	function HookTestHelper_output() {
+		echo "OUTPUT_SUCCESS";
+	}
+	function HookTestHelper_addX($a, $x) {
+		return $a + $x;
+	}
+
+	class HookTest extends PHPUnit_Framework_TestCase {
+		public function testLambdaBinding() {
+			Hooks::bind("lambda_test", function($a) { return $a+1; });
+
+			$value = 2;
+			$value = Hooks::filter("lambda_test", $value);
+			$this->assertEquals($value, 3);
+			
+			Hooks::bind("lambda_test2", function($a, $b) { return $a+$b; });
+
+			$value = 2;
+			$value = Hooks::filter("lambda_test2", $value, 4);
+			$this->assertEquals($value, 6);
+		}
+
+		public function testBind() {	
+			$this->assertTrue(Hooks::bind('bind_test', "HookTestHelper_addOne"));
+			$this->assertFalse(Hooks::bind('bind_test', "bogus_function"));
+		}
+
+		public function testPriorities() {	// tests order and binding by exploiting non-transitivity
+			Hooks::bind("priority_test", "HookTestHelper_addOne", 10);
+			Hooks::bind("priority_test", "HookTestHelper_timesThree", 1);
+
+			$value = 3;	// times three = 9, plus 1 = 10.
+			$value = Hooks::filter("priority_test", $value);
+			$this->assertEquals($value, 10, "Not equal to expected value. If it came out as 12, then the priorities are not being honored (3+1)*3 instead of (3*3)+1.");
+		}
+
+		public function testParameters() {
+			Hooks::bind("parameter_test", "HookTestHelper_addX");
+			$value = 1;
+			$value = Hooks::filter("parameter_test", $value, array(3));
+			$this->assertEquals($value, 4);
+
+			$value = 4;
+			$value = Hooks::filter("parameter_test", $value, 6);
+			$this->assertEquals($value, 10, "Non-array mode parameters didn't work.");
+		}
+
+		public function testMultipleFilter() {
+			Hooks::bind("multiplefilter_test", "HookTestHelper_addOne", 1);
+			Hooks::bind("multiplefilter_test", "HookTestHelper_addOne", 2);
+			Hooks::bind("multiplefilter_test", "HookTestHelper_addOne", 3);
+			$value = 1;
+			$value = Hooks::filter("multiplefilter_test", $value);
+			$this->assertEquals($value, 4, "Different priorities didn't cascade.");
+
+			Hooks::clear();
+			Hooks::bind("multiplefilter_test2", "HookTestHelper_addOne");
+			Hooks::bind("multiplefilter_test2", "HookTestHelper_addOne");
+			Hooks::bind("multiplefilter_test2", "HookTestHelper_addOne");
+			Hooks::bind("multiplefilter_test2", "HookTestHelper_addOne");
+			$value = -1;
+			$value = Hooks::filter("multiplefilter_test2", $value);
+			$this->assertEquals($value, 3, "Same priorities didn't cascade.");
+		}
+
+		public function testExecution() {
+			Hooks::bind("execution_test", "HookTestHelper_output");
+			ob_start();
+				Hooks::run("execution_test");
+			$result = ob_get_clean();
+			$this->assertEquals($result, "OUTPUT_SUCCESS");
+		}
+	
+		public function testFilter() {
+			Hooks::bind("filter_test", "HookTestHelper_addOne");
+			$result = Hooks::filter("filter_test", 14);
+			$this->assertEquals(15, $result);
+		}
+
+		public function testClear() {
+			Hooks::bind("clear_test", "HookTestHelper_addOne");
+			Hooks::clear();
+			$result = Hooks::filter("clear_test", 10);
+			$this->assertEquals($result, 10);
+
+			Hooks::bind("clear_test", "HookTestHelper_addOne");
+			Hooks::clear("clear_test");
+			$result = Hooks::filter("clear_test", 10);
+			$this->assertEquals($result, 10);
+		}
+
+		public function testEmptyHook() {
+			Hooks::clear();
+			Hooks::run("empty_hook");
+			$result = Hooks::filter("empty_hook", "abc");
+			$this->assertEquals("abc", $result);
+		}
+	}
